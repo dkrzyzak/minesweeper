729d19a67942facb452e153c9025376ca32ac0e6
apocalypseDreamer <damntheguitars@gmail.com>
2023-09-22 22:19:06 +0200

    add fonts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import UnFonts from 'unplugin-fonts/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-})
+	plugins: [
+		svelte(),
+		UnFonts({
+			custom: {
+				families: {
+					'Arcade Classic': {
+						src: './src/assets/fonts/ARCADECLASSIC.TTF',
+					},
+					'PC Senior': {
+						src: './src/assets/fonts/pcsenior.ttf',
+					},
+					Rexlia: {
+						src: './src/assets/fonts/rexlia-rg.ttf',
+					},
+					Wheaton: {
+						src: './src/assets/fonts/wheaton-capitals.ttf',
+					},
+				},
+				preload: true,
+				injectTo: 'head-prepend',
+			},
+		}),
+	],
+});
